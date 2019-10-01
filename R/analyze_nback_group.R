656575e79a0a8ce17c49e5c52fc7a3a8f6ff5370
taysgd <53057107+taysgd@users.noreply.github.com>
2019-10-01 15:04:57 -0400

    stuff

--- R/analyze_nback_group.R
+++ R/analyze_nback_group.R
@@ -9,6 +9,17 @@ analyze_nback_group <- function(group_name, subject_paths)
   all_accuracy_data <- vector()
   all_responseTime_data <- vector()
 
+  # this is to create the new folders in a newly created group folder
+  #setwd(subject_paths) # paste0("/",toString(
+  setwd("Young_Adults_Test")
+  dir.create("Figures")
+  dir.create("Processed")
+  setwd("Processed")
+  dir.create("Nback_files")
+  setwd("..")
+  setwd("..")
+  setwd("..")
+
   for (this_subject_path in subject_paths){
     this_subject_path_string_split = strsplit(this_subject_path,"/")[1][1]
     this_subject_id = vapply(this_subject_path_string_split, tail, "", 1)
@@ -26,15 +37,16 @@ analyze_nback_group <- function(group_name, subject_paths)
     setwd("..")
   }
 
-  write.csv(all_accuracy_data, file = file.path("Group_Results/Nback_files", paste0(toString(group_name), "_accuracy",".csv")))
 
-  write.csv(all_responseTime_data, file = file.path("Group_Results/Nback_files", paste0(toString(group_name), "_responseTime",".csv")))
+  write.csv(all_accuracy_data, file = file.path("Young_Adults/Group_Results/Nback_files", paste0(toString(group_name), "_accuracy",".csv")))
+
+  write.csv(all_responseTime_data, file = file.path("Young_Adults/Group_Results/Nback_files", paste0(toString(group_name), "_responseTime",".csv")))
 
   all_accuracy_data_averaged <- aggregate(all_accuracy_data["subject_accuracy"], by=list(all_accuracy_data$ISI, all_accuracy_data$nback),FUN=mean)
   colnames(all_accuracy_data_averaged) <- c("ISI", "nback", "averaged_accuracy")
 
   accuracy_file_name_pdf = paste0(toString(group_name), "_Accuracy",".pdf")
-  file = file.path("Group_Results/Figures", accuracy_file_name_pdf)
+  file = file.path("Young_Adults/Group_Results/Figures", accuracy_file_name_pdf)
   accuracy_fig = ggplot(data=all_accuracy_data_averaged, aes(fill = ISI, x = nback, y=averaged_accuracy)) + geom_bar(position = "dodge", stat = "identity")
   accuracy_fig + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                        panel.background = element_blank(), axis.line = element_line(colour = "black")) + ggtitle("Subject Accuracy for N-Back Levels and ISI") + xlab("N-Back Level") + ylab("Percent Correct (%)") +
@@ -42,7 +54,7 @@ analyze_nback_group <- function(group_name, subject_paths)
   ggsave(file)
 
   accuracy_file_name_jpeg = paste0(toString(group_name), "_Accuracy",".jpeg")
-  file = file.path("Group_Results/Figures", accuracy_file_name_jpeg)
+  file = file.path("Young_Adults/Group_Results/Figures", accuracy_file_name_jpeg)
   accuracy_fig = ggplot(data=all_accuracy_data_averaged, aes(fill = ISI, x = nback, y=averaged_accuracy)) + geom_bar(position = "dodge", stat = "identity")
   accuracy_fig + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                        panel.background = element_blank(), axis.line = element_line(colour = "black")) + ggtitle("Subject Accuracy for N-Back Levels and ISI") + xlab("N-Back Level") + ylab("Percent Correct (%)") +
@@ -50,14 +62,14 @@ analyze_nback_group <- function(group_name, subject_paths)
   ggsave(file)
 
   responsetime_file_name_pdf = paste0(toString(group_name), "_ResponseTime",".pdf")
-  file = file.path("Group_Results/Figures", responsetime_file_name_pdf)
+  file = file.path("Young_Adults/Group_Results/Figures", responsetime_file_name_pdf)
   ggplot(all_responseTime_data, aes(x = factor(nback_level_correct), y = subject_response_onset_correct)) + geom_violin(aes(fill = factor(interstimulus_interval_correct))) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.title = element_blank()) +
     scale_fill_manual(values=c("orange","blue"))  + ggtitle("Reaction Time for N-Back Levels and ISI") + xlab("N-Back Level") + ylab("Onset Time (ms)")
   ggsave(file)
 
    responsetime_file_name_jpeg = paste0(toString(group_name), "_ResponseTime",".jpeg")
-   file = file.path("Group_Results/Figures", responsetime_file_name_jpeg)
+   file = file.path("Young_Adults/Group_Results/Figures", responsetime_file_name_jpeg)
    ggplot(all_responseTime_data, aes(x = factor(nback_level_correct), y = subject_response_onset_correct)) + geom_violin(aes(fill = factor(interstimulus_interval_correct))) +
      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.title = element_blank()) +
      scale_fill_manual(values=c("orange","blue"))  + ggtitle("Reaction Time for N-Back Levels and ISI") + xlab("N-Back Level") + ylab("Onset Time (ms)")
